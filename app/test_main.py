3c5fc8da5876128c9e10a4a92067f2489978d5f5
Oleg <oleggavelya@gmail.com>
2026-04-28 10:38:29 +0300

    refactor flake8 line to long

--- app/test_main.py
+++ app/test_main.py
@@ -3,8 +3,7 @@ import pytest
 
 
 class TestAnimal:
-    @pytest.mark.parametrize("cat_age, dog_age, expected",
-    [
+    @pytest.mark.parametrize("cat_age, dog_age, expected", [
         (0, 0, [0, 0]),
         (16, 0, [1, 0]),
         (14, 14, [0, 0]),
@@ -14,9 +13,13 @@ class TestAnimal:
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
         (100, 100, [21, 17]),
-    ]
-    )
-    def test_should_check_age_animal_is_human(self, cat_age: int, dog_age: int, expected: list) -> None:
+    ])
+    def test_should_check_age_animal_is_human(
+            self,
+            cat_age: int,
+            dog_age: int,
+            expected: list
+    ) -> None:
         assert get_human_age(cat_age, dog_age) == expected
 
     def test_should_raise_type_error(self) -> None:
